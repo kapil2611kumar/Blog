4a429615cfeba2c917cd45f812ce8d244a8ab4da
kapil2611kumar <102678879+kapil2611kumar@users.noreply.github.com>
2022-06-24 00:56:12 +0530

    complete

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { heading } from '../style/homeStyle'
+import { motion } from 'framer-motion'
 
-const Heading = () => <h1 style ={heading}> THE BLOG </h1>
+const Heading = () => <motion.h1 style ={heading} initial={{x:-200,opacity:0}} animate={{x:0 ,opacity:1}} 
+transition ={{duration :2}}>  
+THE BLOG </motion.h1>
 
 export default Heading 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
-import {icons} from "../style/global";
+import {colors, icons} from "../style/global";
+import {motion} from "framer-motion";
 import { Link } from 'react-router-dom';
 import { links, logo, navbar, signs,icon } from '../style/homeStyle';
 
 function Navbar() {
   return (
-    <nav style ={navbar}>
-        <div style={logo}>
+    <motion.nav style ={navbar} initial ={{y:-50,opacity:0}}
+     animate={{y:0,opacity:1}} transition={{duration:2}}>
+        <motion.div style={logo}   initial ={{y:-50,opacity:0}}
+     animate={{y:0,opacity:1}} transition={{duration:1,delay:1}}>
             <h1>D</h1>
-        </div>
+        </motion.div>
 
-        <div style={links}>
-         <Link to ="/"> <span className="link">Home</span> </Link>
-         <Link to ="/create"> <span className="link">Create a Blog</span></Link>
-        </div>
+        <motion.div style={links}  initial ={{y:-50,opacity:0}}
+     animate={{y:0,opacity:1}} transition={{duration:1,delay:2}}>
+         <Link to ="/" style ={{color:colors.text,textDecoration:"none"}}> <span className="link">Home</span> </Link>
+         <Link to ="/create" style ={{color:colors.text,textDecoration:"none"}}> <span className="link">Create a Blog</span></Link>
+        </motion.div>
 
         <div style={signs}>
             <span style={ icon}>{icons.twitter}</span>
             <span style= {icon}>{icons.insta}</span>
             <span style={ icon}>{icons.blogIcon}</span>
         </div>
-    </nav>
+    </motion.nav>
   )
 }
 
